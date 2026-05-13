64624b95917b3a6dbac7ad0982ae4335fc0f0a00
Jayraj <jayrajdhakan2001@gmail.com>
2026-05-13 16:03:55 +0530

    one time download logic added

--- file_sharing_app/views.py
+++ file_sharing_app/views.py
@@ -7,32 +7,45 @@ def home(request):
     # user uploads the file and it is stored in database
     if request.method == 'POST' and request.FILES.get('file'):
         uploaded_file = request.FILES['file']
-        # Create the database record
         new_file = SharedFile.objects.create(file=uploaded_file)
-
-        # creates a download link of the file that user uploads
         share_url = f"{request.build_absolute_uri('/')}download/{new_file.id}/"
-
         return render(request, 'file_sharing_app/home.html', {'share_url': share_url})
     
     return render(request, 'file_sharing_app/home.html')
+
 
 def download_file(request, file_id):    
     # finds the file in the database
     shared_file = get_object_or_404(SharedFile, id=file_id)
     
     if shared_file.is_expired():
-        # if file is expired the file gets deleted from the database
+        # Clean up the actual file from the media folder, then delete DB record
+        if shared_file.file:
+            shared_file.file.delete(save=False)
         shared_file.delete()
         return HttpResponse("This file link has expired and was deleted.", status=410)
     
-    # 1. If the user clicks the "Download" button, serve the actual file
+    # 1. If the user clicks the "Download" button
     if request.GET.get('action') == 'download':
-        response = HttpResponse(shared_file.file, content_type='application/octet-stream')
-        response['Content-Disposition'] = f'attachment; filename="{os.path.basename(shared_file.file.name)}"'
+        # Read the file data into memory so we can delete the actual file safely
+        file_data = shared_file.file.read()
+        file_name = os.path.basename(shared_file.file.name)
+        
+        # Prepare the file to be sent to the user
+        response = HttpResponse(file_data, content_type='application/octet-stream')
+        response['Content-Disposition'] = f'attachment; filename="{file_name}"'
+        
+        #  ONE-TIME DOWNLOAD LOGIC 
+        # Delete the actual file from the "uploads" folder
+        if shared_file.file:
+            shared_file.file.delete(save=False)
+        # Delete the record from the database
+        shared_file.delete()
+        
+        # Send the file to the user
         return response
     
-    # 2. Otherwise, just show the landing page with the file details
+    # 2. Otherwise, just show the landing page
     context = {
         'shared_file': shared_file,
         'filename': os.path.basename(shared_file.file.name)
